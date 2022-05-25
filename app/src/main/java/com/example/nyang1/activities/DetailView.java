165ff825b9c7a19ec556ff51ddafe9e181ee6e62
inha <bse07210@naver.com>
2022-05-25 13:52:10 +0900

    상세정보 Parcelable 객체로 넘기기

--- app/src/main/java/com/example/nyang1/activities/DetailView.java
+++ app/src/main/java/com/example/nyang1/activities/DetailView.java
@@ -1,10 +1,43 @@
 package com.example.nyang1.activities;
 
-public class DetailView {
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.TextView;
 
-    public void setplace_name(String place_name){}
-    public void setdistance(String distance){}
-    public void setplace_url(String place_url){}
+import androidx.appcompat.app.AppCompatActivity;
 
+import com.example.nyang1.R;
+import com.example.nyang1.category_search.Document;
+import com.example.nyang1.utils.IntentKey;
+
+public class DetailView extends AppCompatActivity {
+
+    TextView placeNameText;
+    TextView addressText;
+    TextView roadAddressName;
+    TextView urlText;
+    TextView phoneText;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.detail);
+
+        placeNameText = findViewById(R.id.placeName);
+        addressText = findViewById(R.id.addressName);
+        roadAddressName = findViewById(R.id.roadAddressName);
+        urlText = findViewById(R.id.urlText);
+        phoneText = findViewById(R.id.phone);
+        processIntent();
+    }
+    private void processIntent(){
+        Intent processIntent = getIntent();
+        Document document = processIntent.getParcelableExtra(IntentKey.PLACE_SEARCH_DETAIL_EXTRA);
+        placeNameText.setText(document.getPlaceName());
+        addressText.setText(document.getAddressName());
+        roadAddressName.setText(document.getRoadAddressName());
+        urlText.setText(document.getPlaceUrl());
+        phoneText.setText(document.getPhone());
+    }
 
 }
